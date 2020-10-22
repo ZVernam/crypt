8dd4e9a9489e7a7ecc869f1aa1f7a5978bbfefe1
Eugene Shchepotev <evgenii.schepotiev@gmail.com>
2020-10-22 21:34:40 +0000

    fix hello worlds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import Base64 from 'crypto-js/enc-base64';
+import utf8 from 'crypto-js/enc-utf8';
 
-console.log(Base64.stringify(`Hello, crypto-js!`));
+
+console.log(Base64.stringify(utf8.parse(`Hello, crypto-js!`)));
