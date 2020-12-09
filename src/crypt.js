a7f93f7027d6e6e0a43502f2850495c8b0266bdf
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-12-09 18:00:03 +0300

    update crypt

--- src/crypt.js
+++ src/crypt.js
@@ -1,12 +1,13 @@
-import Base64 from 'crypto-js/enc-base64';
 import utf8 from 'crypto-js/enc-utf8';
 import aesCipher from 'crypto-js/aes';
 
 // Encrypt
 const MESSAGE = 'my message';
 const KEY = 'secret key 123';
-const ciphertext = aesCipher.encrypt(MESSAGE, KEY);
-console.log(Base64.stringify(ciphertext))
+const cipher = aesCipher.encrypt(MESSAGE, KEY);
+console.log(cipher);
+const ciphertext = cipher.toString();
+console.log(ciphertext);
 
 // Decrypt
 const bytes  = aesCipher.decrypt(ciphertext, KEY);
