0fc0a0fbb5b84837ed3a11a1b0a0456a07e9720c
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-12-10 20:07:32 +0300

    get elements

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import utf8 from 'crypto-js/enc-utf8';
 
 import './crypt';
 
+const plainTextElement = document.querySelector(`#text`);
+const secretTextElement = document.querySelector(`#secret`);
+const cipherTextElement = document.querySelector(`#cipher`);
+
+const encryptButton = document.querySelector(`#encrypt`);
+
+const onInputChange = () => {
+  const plaintText = plainTextElement.value;
+  const secretText = secretTextElement.value;
+  encryptButton.disabled = !(plaintText && secretText);
+};
+
+plainTextElement.addEventListener(`input`, onInputChange);
+secretTextElement.addEventListener(`input`, onInputChange);
+
+onInputChange();
+
 console.log(Base64.stringify(utf8.parse(`Hello, crypto-js!`)));
