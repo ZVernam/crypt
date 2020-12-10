dfe90c3099d870969eae4d587cfec0efa1ef1a9b
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-12-10 18:32:52 +0300

    add openssl encrypt example

--- encrypt.sh
+++ encrypt.sh
@@ -1,14 +1,8 @@
-# crypt
-Encrypt/decrypt whatever online without SMS and registration
+#!/usr/bin/env bash
 
-#### OpenSSL encrypt/decrypt
-
-Encrypt:
-```shell script
 echo -n "$@" | # input text
  openssl enc \
   -e `# encode` \
   -aes-256-cbc `# ciphername` \
   -base64 `# base64` \
   -salt # use salt (randomly generated or provide with -S option) when encrypting (this is the default).
-```
