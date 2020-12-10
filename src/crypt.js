289774b9c0c3a3c2b805536580d70293e5345a9f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-12-10 20:20:00 +0300

    add info

--- src/crypt.js
+++ src/crypt.js
@@ -1,6 +1,15 @@
 import utf8 from 'crypto-js/enc-utf8';
 import aesCipher from 'crypto-js/aes';
 
+/*
+ * AES Cipher defaults:
+ * Cipher: AES-256-cbc
+ * Salt: Random and packed to response
+ * Key, IV: Derived from password (See https://www.openssl.org/docs/manmaster/man3/EVP_BytesToKey.html)
+ * Output: base64 string with OpenSSL salted format (See http://justsolve.archiveteam.org/wiki/OpenSSL_salted_format)
+ */
+
+
 export const encrypt = (message, password) => aesCipher.encrypt(message, password).toString();
 
 export const decrypt = (ciphertext, password) => aesCipher.decrypt(ciphertext, password).toString(utf8);
